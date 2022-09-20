a19f2253c2642be2663c32da76707283683b629e
li-wh <li-wh19@mails.tsinghua.edu.cn>
2022-09-21 00:14:01 +0800

    hide sidebar title

--- source/conf.py
+++ source/conf.py
@@ -52,6 +52,9 @@ html_theme = 'furo'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_theme_options = {
+    "sidebar_hide_name": True,
+}
 
-html_title = ""
+html_title = "Sereinme"
 html_logo = "_static/logo.svg"
